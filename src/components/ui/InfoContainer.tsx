95649421b9142dd6976cc8c82ef172a491fd15da
Ivowainer <ivancamposwainer@gmail.com>
2022-09-02 10:25:46 -0300

    Fix

--- src/components/ui/InfoContainer.tsx
+++ src/components/ui/InfoContainer.tsx
@@ -12,7 +12,7 @@ const InfoContainer: React.FC<InfoContainerProps> = ({ country }) => {
   return (
     <div className="mt-14 sm:grid sm:grid-cols-1 lg:grid-cols-2 md:gap-24">
       <Image src={flags?.svg || flags?.png || ""} width={450} height={270} layout="responsive" alt={`Image of the country ${name?.common}`}></Image>
-      {/* <InfoCountry country={country}/> */}
+      <InfoCountry country={country}/>
     </div>
   )
 }
