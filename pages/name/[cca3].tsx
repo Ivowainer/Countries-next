413db9079660de17de73c1ddd7c811c362145e22
Ivowainer <ivancamposwainer@gmail.com>
2022-09-07 20:29:56 -0300

    Update cca3

--- pages/name/[cca3].tsx
+++ pages/name/[cca3].tsx
@@ -3,6 +3,8 @@ import MainLayout from "../../src/components/layouts/MainLayout";
 import { countriesApi } from "../../src/helpers/countriesApi"
 import { CountriesAll } from '../../src/interfaces/CountriesAll';
 
+import Link from 'next/link'
+
 import { BsArrowLeftShort } from "react-icons/bs"
 import InfoContainer from "../../src/components/ui/InfoContainer";
 
@@ -13,11 +15,11 @@ const CountryName: NextPage<CountryNameProps> = ({ country }) => {
     return (
         <MainLayout page={`${country?.name?.common} | Countries`}>
             <div className="px-8 py-8 md:px-20 md:py-12">
-                <button 
-                    className="px-12 py-2 rounded dark:bg-slate-800 bg-white shadow-md hover:shadow-xl transition-shadow duration-700 flex justify-center items-center"
-                >
-                    <BsArrowLeftShort />Back
-                </button>
+                <Link href="/">
+                    <a  className="px-12 py-2 rounded w-full md:w-32 dark:bg-slate-800 bg-white shadow-md hover:shadow-xl transition-shadow duration-700 flex justify-center items-center">
+                        <BsArrowLeftShort />Back
+                    </a>
+                </Link>
                 <InfoContainer country={country}/>
             </div>
         </MainLayout>
